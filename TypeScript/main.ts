cae6cee4018be8066a512e73e8ecce5afff52e8f
Amin Ehsan <Mr.AminEhsan@gmail.com>
2023-07-01 21:23:52 +0330

    Create main.ts

---
+++ TypeScript/main.ts
@@ -0,0 +1,51 @@
+class Target {
+    public request(): string {
+        return "Target: The default target's behavior.";
+    }
+}
+
+class Adaptee {
+    public specificRequest(): string {
+        return ".eetpadA eht fo roivaheb laicepS";
+    }
+}
+
+class Adapter extends Target {
+    private adaptee: Adaptee;
+
+    constructor(adaptee: Adaptee) {
+        super();
+        this.adaptee = adaptee;
+    }
+
+    public request(): string {
+        const result = this.adaptee.specificRequest().split("").reverse().join("");
+        return `Adapter: (TRANSLATED) ${result}`;
+    }
+}
+
+function clientCode(target: Target) {
+    console.log(target.request());
+}
+
+function main() {
+    console.log("Client: I can work just fine with the Target objects:");
+    const target = new Target();
+    clientCode(target);
+
+    console.log("");
+
+    const adaptee = new Adaptee();
+    console.log(
+        "Client: The Adaptee class has a weird interface. See, I don't understand it:"
+    );
+    console.log(`Adaptee: ${adaptee.specificRequest()}`);
+
+    console.log("");
+
+    console.log("Client: But I can work with it via the Adapter:");
+    const adapter = new Adapter(adaptee);
+    clientCode(adapter);
+}
+
+main();
